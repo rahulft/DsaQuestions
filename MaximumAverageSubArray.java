d3dfa5485babd885d8b0a0295baef8e5b57198aa
rahulft <108999105+rahulft@users.noreply.github.com>
2025-11-03 22:14:03 +0530

    Add files via upload
    🔹 The idea again
    
    We are moving a sliding window of size k through the array.
    
    At every new index i,
    
    nums[i] → is the new element entering the window (right side).
    
    nums[i - k] → is the old element leaving the window (left side).
    
    🧮 Example:
    
    Let’s use:
    
    nums = [1, 12, -5, -6, 50, 3]
    k = 4
    
    Step-by-step explanation of nums[i - k]
    
    When we start sliding:
    
    We already calculated the sum for the first window [1, 12, -5, -6]
    
    That’s indices 0, 1, 2, 3
    
    Now i will start from k, i.e. i = 4.

---
+++ MaximumAverageSubArray.java
@@ -0,0 +1,19 @@
+lass Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double sum  = 0 ;
+    double maxSum=0 ;
+        for( int i  =0 ; i<k  ;i++){
+            sum += nums[i];
+        }
+        maxSum = sum;
+
+        for(int i = k ; i<nums.length; i++ ){
+            sum = sum + nums[i] - nums[i - k];
+            maxSum = Math.max(sum , maxSum);
+
+        }
+        return maxSum/k;
+
+        
+    }
+}
