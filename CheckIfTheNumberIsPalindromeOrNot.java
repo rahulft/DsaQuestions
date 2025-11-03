322913fa1e9e395e4fccd51fabf78167b96f663d
rahulft <108999105+rahulft@users.noreply.github.com>
2025-11-03 21:23:32 +0530

    Add files via upload
    Using Reursion

---
+++ CheckIfTheNumberIsPalindromeOrNot.java
@@ -0,0 +1,11 @@
+public class CheckIfTheNumberIsPalindromeOrNot {
+    
+    public boolean checkTheNumber( int number){
+        if ( number < 0){
+            return false;
+            
+        }
+        if( x != 0 )
+    }
+    
+}
